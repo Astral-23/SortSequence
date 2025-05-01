99b0311833b4053b61f146b41b8c13ac53882dc4
Astral-23 <ul7cr38@gmail.com>
2025-05-01 23:20:07 +0900

    epochカウントを1-indexedに

--- src/util.py
+++ src/util.py
@@ -2,9 +2,9 @@ import torch
 import copy
 
 def train_net(model, crit, opt, epochs, dl_train, dl_val):
-    best_w, best_loss = None, 1e9
+    best_w, best_loss = None, float('inf')
     for ep in range(epochs):
-        print(f"Epoch {ep}/{epochs-1}")
+        print(f"Epoch {ep+1}/{epochs}")
         for phase, dl in [("train", dl_train), ("val", dl_val)]:
             model.train() if phase == "train" else model.eval()
             total_loss = 0
